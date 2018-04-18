e309e72c09cfe8c10d717798501d81aa0d288992
JosephM2014 <josephgyonga@yahoo.com>
2018-04-18 09:38:43 +0300

    initial commit-2018-18April

---
+++ app/src/main/java/apps/yonga/com/tictactoe2/About.java
@@ -0,0 +1,13 @@
+package apps.yonga.com.tictactoe2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
