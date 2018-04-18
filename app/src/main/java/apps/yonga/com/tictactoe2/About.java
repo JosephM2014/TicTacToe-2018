cffe2a82bb86f34d232ff63b11f312b40d2034f6
JosephM2014 <josephgyonga@yahoo.com>
2018-04-18 21:00:47 +0300

    Initial2

--- app/src/main/java/apps/yonga/com/tictactoe2/About.java
+++ app/src/main/java/apps/yonga/com/tictactoe2/About.java
@@ -8,6 +8,6 @@ public class About extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_about2);
     }
 }
